61c41918a7acb8d91348ced49104654bfc6f8890
Jeff Sapotille <you@example.com>
2019-04-14 17:34:32 +0200

    ajout des fichiers

---
+++ index.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html"); 
+});
+
+app.post('/', function (req, res) {
+    
+    console.log(req.body.crypto);
+});
+
+app.listen(3000, function () {
+    console.log("Server start"); 
+});
