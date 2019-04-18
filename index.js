717f6d3357c443c1f5c56159ac3baa6f8bbd03c2
Jeff Sapotille <you@example.com>
2019-04-19 00:49:15 +0200

    ajout de option dans la requete

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
 
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+
 const app = express();
 
 
@@ -14,36 +16,52 @@ app.get("/", function (req, res) {
 
 
 
-// utilisation de npm request.
 
 app.post('/', function (req, res) {
-
+    
     let crypto = req.body.crypto;
     //console.log(crypto);
     
     let fiat = req.body.fiat;
     //console.log(fiat);
-
-    let baseUrl = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
-
-    let finalUrl = baseUrl + crypto + fiat ;  
+    
+    let amout = req.body.amout;
+    //console.log(amout);
+    
+    
+    // let baseUrl = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
+    
+    //let finalUrl = baseUrl + crypto + fiat ;  
     //console.log(finalUrl);
+
     
+    let option = {
+        url: 'https://apiv2.bitcoinaverage.com/convert/global',
+        method: 'GET',
+        qs:{
+            from:crypto,
+            to:fiat,
+            amout:amout
+        }
+    };
+    //console.log(option);
     
-    request(finalUrl, function (error, response, body) {
+    // utilisation de npm request.
+    request(option, function (error, response, body) {
         
         //console.log(body);
 
         let data = JSON.parse(body);
-        let price = data.last;
+        let price = data.price;
 
-       let date = data.display_timestamp;  
+        let date = data.time;  
         
         res.write('<p>date du jour ' + date + '</p>');
 
-        res.write('<h1> Le prix du ' + crypto  + ' est : ' + price  + fiat + '</h1>');
+        res.write('<h1>' + amout + crypto  + ' vaut actuellement  ' + price  + fiat + '</h1>');
 
-        //res.send();
+        res.send();
+        
         //console.log(price);    
        // console.log(response.statusCode);//permet d'avoir un code d'etat d'une  html requete.
     });
