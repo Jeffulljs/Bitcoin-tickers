666f0cdee7aa9c1f1edab65d5dde06747110a989
Jeff Sapotille <you@example.com>
2019-04-18 13:07:16 +0200

    ajout de  res.write

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-
-
-
 const app = express();
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -14,23 +12,42 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html"); 
 });
 
+
+
 // utilisation de npm request.
 
 app.post('/', function (req, res) {
+
+    let crypto = req.body.crypto;
+    //console.log(crypto);
     
-    request('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD', function (error, response, body) {
+    let fiat = req.body.fiat;
+    //console.log(fiat);
+
+    let baseUrl = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
+
+    let finalUrl = baseUrl + crypto + fiat ;  
+    //console.log(finalUrl);
+    
+    
+    request(finalUrl, function (error, response, body) {
         
         //console.log(body);
 
         let data = JSON.parse(body);
         let price = data.last;
 
-        console.log(price);
+       let date = data.display_timestamp;  
         
+        res.write('<p>date du jour ' + date + '</p>');
 
-        
+        res.write('<h1> Le prix du ' + crypto  + ' est : ' + price  + fiat + '</h1>');
+
+        //res.send();
+        //console.log(price);    
        // console.log(response.statusCode);//permet d'avoir un code d'etat d'une  html requete.
     });
+
         
 });
 
